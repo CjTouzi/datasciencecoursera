9fe6306517acfb19c7c60f12d437a992c63ace5c
CjTouzi <cjtouzi@gmail.com>
2014-12-06 23:23:02 +0800

    data.table package

--- GC_Data/src/week1.R
+++ GC_Data/src/week1.R
@@ -55,6 +55,7 @@ sort(teams)
 
 
 # json --------------------------------------------------------------------
+
 library(jsonlite)
 jsonData <- fromJSON("https://api.github.com/users/jtleek/repos")
 names(jsonData)
@@ -66,4 +67,62 @@ cat(myjson)
 iris2 <- fromJSON(myjson)
 head(iris2)
 head(jsonData)
- 
+
+
+# data.table package ------------------------------------------------------
+library(data.table)
+DF = data.frame(x=rnorm(9), y=rep(c("a", "b", "c"), each = 3), z=rnorm(9))
+head(DF)
+DT= data.table(x=rnorm(9), y=rep(c("a", "b", "c"), each = 3), z=rnorm(9))
+head(DT)
+# see all the data tables in memory
+tables()
+
+# subsetting rows
+DT[2,]
+DT[DT$y == "a" ]
+DT[c(2,3)]
+
+# calculating values for variables with expressions
+DT[, list(mean(x),sum(z))]
+DT[, table(y)]
+
+# adding new columns :=
+DT[, w:=z^2]
+DT
+DT2 <- DT
+DT[, y:=2]
+head(DT, n=3)
+head(DT2, n=3)
+
+# multiple operations
+DT[, m:={tmp<-(x+z); log2(tmp+5)}]
+# plyr like operations
+DT[, a:=x>0]
+DT
+DT[,b:=mean(x+w), by=a]
+DT
+
+# special variables
+# .N An integer, length 1, containing the number r
+set.seed(123)
+DT <- data.table(x=sample(letters[1:3], 1E5, TRUE))
+DT[, .N, by=x]
+
+# the keys of data table 
+DT <- data.table(x=rep(c("a","b","c"), each =100), y=rnorm(300))
+setkey(DT,x)
+DT['a']
+
+# join between two tables
+DT1 <- data.table(x=c('a','b','c','dt1'), y=1:4)
+DT2 <- data.table(x=c('a','b','c','dt2'), z=5:7)
+setkey(DT1, x); setkey(DT2, x)
+merge(DT1,DT2)
+
+# fast reading
+big_df <- data.frame(x=rnorm(1E6), y=rnorm(1E6))
+file <- tempfile()
+write.table(big_df, file=file, row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
+system.time(fread(file))
+system.time(read.table(file, header=TRUE, sep="\t"))
